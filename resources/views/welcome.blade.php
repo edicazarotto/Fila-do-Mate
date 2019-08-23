76a171c2dd1ccd81f14fff4f82e6f628e16e2322
lffruhling <lffruhling@hotmail.com>
2019-08-22 21:23:15 -0300

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fila do Mate</title>
+</head>
+<body>
+
+    <nav>Cabeçalho</nav>
+
+    @yield('conteudo')
+
+
+    <footer>Rodapé</footer>
+
+</body>
+</html>
